f184b89cd9c69f805c66c8078f0703038e8b55f7
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-10-16 17:36:57 -0500

    chore(docker-compose): add celery and app sevices

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost:5432/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
