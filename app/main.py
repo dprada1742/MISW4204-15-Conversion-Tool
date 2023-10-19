2a1f2b12599f76848c2232da4b43fde97d32305e
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-10-19 01:17:11 -0500

    chore: run migrations

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.database import run_migrations
 from app.routers import auth, tasks
 
 app = FastAPI()
+
+run_migrations()
 
 app.include_router(auth.router)
 app.include_router(tasks.router)
