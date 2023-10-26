901200e82e0600ef5944ef866839ccca8bdd5d11
Sebastian Ulloa <js.ulloa1943@uniandes.edu.co>
2023-10-25 21:41:00 -0500

    updated redis credentials

--- app/celery_app.py
+++ app/celery_app.py
@@ -7,8 +7,8 @@ from app.models import TaskStatus
 
 celery_app = Celery(
     "conversion_tool",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0",
+    broker="redis://default:strong-password:34.171.232.213:6379/0",
+    backend="redis://default:strong-password:34.171.232.213:6379/0",
 )
 
 
