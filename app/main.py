a96fbf752c242aab0e18368f13f192e0ef4fa34e
David Sánchez <dc.sanchezm1@uniandes.edu.co>
2023-10-26 00:04:37 -0500

    feat: add exception handler

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from app.database import run_migrations
 from app.routers import auth, tasks
+import logging
+
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+
+@app.exception_handler(Exception)
+async def generic_exception_handler(request: Request, exc: Exception):
+    logger.error(f"An error occurred: {exc}")
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "An unexpected error occurred."},
+    )
+
 
 run_migrations()
 
